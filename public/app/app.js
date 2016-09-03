f36779631f0482d27c0b6cc0403f17a8863cecc4
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2016-09-03 02:44:49 -0500

    Implementacion de Modulo Nomina

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-//var app = angular.module('softver-aqua', []).constant('API_URL', 'http://localhost:88/aqua/public/');
+var app = angular.module('softver-aqua', ['ngPagination']).constant('API_URL', 'http://localhost:88/aqua/public/');
 
-var app = angular.module('softver-aqua', []).constant('API_URL', 'http://localhost:8000/');
-=======
-var app = angular.module('softver-aqua', ['ngPagination']).constant('API_URL', 'http://localhost:88/aqua/public/'); 
->>>>>>> 3d95cd5dc502e4e963c53cf198de8f4147af510e
