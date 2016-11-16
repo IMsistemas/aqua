7c39ea55787d984f6dbeff62514a89b87aeead5b
christianmr <eelmetalero89@hotmail.com>
2016-11-16 10:20:07 -0500

    Solicitud1
    Solicitud1

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -92,7 +92,7 @@ app.controller('mainController',['$scope','$route', function($scope, $http, API_
 
 	$scope.toModuloSolicitud = function(){	
 		$scope.titulo = "Solicitudes";
-		$scope.toModulo = "suministros/solicitud";
+		$scope.toModulo = "solicitud";
 	}
 	$scope.toModuloSolicitudEspera = function(){	
 		$scope.titulo = "Solicitudes";
