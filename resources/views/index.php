0a9207d6ae01a9c5954b3b96dc54cd0ac959dabe
christianmr <eelmetalero89@hotmail.com>
2016-12-27 11:33:32 -0500

    contablilidad 4
    contablilidad 4

--- resources/views/index.php
+++ resources/views/index.php
@@ -194,8 +194,8 @@
     <script src="<?= asset('app/controllers/cpClienteController.js') ?>"></script>
     <script src="<?= asset('app/controllers/comprasproductoController.js') ?>"></script>
     <script src="<?= asset('app/controllers/comprasproductoIngresoController.js') ?>"></script>
-    <script src="<?= asset('app/controllers/comprarImprimirController.js') ?>"></script>
-    <script src="<?= asset('app/controllers/retencionCompraController.js') ?>"></script>
+    <script src="<?= asset('app/controllers/comprasImprimirController.js') ?>"></script>
+    <script src="<?= asset('app/controllers/retencionCompra.js') ?>"></script>
 
     
 
