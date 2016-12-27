8a53110ca9e7311f2d85adc37b4655854212afb3
christianmr <eelmetalero89@hotmail.com>
2016-12-27 12:03:40 -0500

    compras 3
    compras 3

--- resources/views/compras/listado_compras.php
+++ resources/views/compras/listado_compras.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US" ng-app="softver-aqua-page">
+    <head>
+        <title>Compras Inventario</title>
 
+        <!-- Load Bootstrap CSS -->
+        <link href="<?= asset('css/bootstrap.min.css') ?>" rel="stylesheet">
+        <link href="<?= asset('css/font-awesome.min.css') ?>" rel="stylesheet">
+        <link href="<?= asset('css/index.css') ?>" rel="stylesheet">
+        <link href="<?= asset('css/bootstrap-datetimepicker.min.css') ?>" rel="stylesheet">
+        <link href="<?= asset('css/style_generic_app.css') ?>" rel="stylesheet">
+        <link href="<?= asset('css/angucomplete-alt.css') ?>" rel="stylesheet">
+    </head>
+    <body>
     <div ng-controller="comprasproductoController">
     
     <div class="container">
@@ -180,4 +193,20 @@
     </div>
     </div>
     
-       
+        <!-- Load Javascript Libraries (AngularJS, JQuery, Bootstrap) -->
+    <script src="<?= asset('app/lib/angular/angular.min.js') ?>"></script>
+    <script src="<?= asset('app/lib/angular/angular-route.min.js') ?>"></script>
+    <script src="<?= asset('app/lib/angular/dirPagination.js') ?>"></script>
+    
+    <script src="<?= asset('app/lib/angular/ng-file-upload-shim.min.js') ?>"></script>
+    <script src="<?= asset('app/lib/angular/ng-file-upload.min.js') ?>"></script>   
+    <script src="<?= asset('app/lib/angular/angucomplete-alt.min.js') ?>"></script>    
+    
+       <script src="<?= asset('js/jquery.min.js') ?>"></script>
+        <script src="<?= asset('js/bootstrap.min.js') ?>"></script>
+        
+        <!-- AngularJS Application Scripts -->
+        <script src="<?= asset('app/app.js') ?>"></script>
+        <script src="<?= asset('app/controllers/comprasproductoController.js') ?>"></script>
+    </body>
+</html>
