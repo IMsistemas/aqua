b236b7993eac7319bde4e00d1530ee4de5fd1efa
christianmr <eelmetalero89@hotmail.com>
2016-12-28 14:34:31 -0500

    Ventas 3
    Ventas 3

--- resources/views/Facturacionventa/index.php
+++ resources/views/Facturacionventa/index.php
@@ -8,9 +8,7 @@
     <div ng-show="ActivaVenta=='0'" ng-hide="ActivaVenta=='1'">
       <div class="row">
         <hr/>
-        <div class="col-xs-12 text-center">
-          <h3><strong>Registro Ventas</strong></h3>
-        </div>
+        
         <div class="col-xs-2">
           
           <div class="input-group ">
