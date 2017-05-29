1637a53dd6c176374df7b31348fcd1eaabae35f9
christianmr <eelmetalero89@hotmail.com>
2017-05-29 14:29:11 -0500

    AF
    AF

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -260,7 +260,7 @@ app.controller('mainController',function($scope, $http, API_URL) {
 
         $scope.toModuloActivosFijos = function(){
         $scope.titulo = "Crear Transportistas";
-        $scope.toModulo = "Activosfijos";
+        $scope.toModulo = "Activosfijos/activosfijos";
     }
 
     $scope.prepareListBreadcrumb = function (list_module) {
