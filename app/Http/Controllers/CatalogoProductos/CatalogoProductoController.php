1641eee9023d7c5ad00491f3726d94d164d15b82
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2017-07-30 18:30:26 -0500

    Refactoring

--- app/Http/Controllers/CatalogoProductos/CatalogoProductoController.php
+++ app/Http/Controllers/CatalogoProductos/CatalogoProductoController.php
@@ -337,7 +337,7 @@ class CatalogoProductoController extends Controller
     	return  Cont_CatalogItem::orderBy('codigoproducto', 'asc')
     	->join('cont_categoria', 'cont_categoria.idcategoria', '=', 'cont_catalogitem.idcategoria')
         ->join('cont_claseitem', 'cont_claseitem.idclaseitem', '=', 'cont_catalogitem.idclaseitem')
-    	->whereRaw("( idcatalogitem::text like '%" . $filter->text . "%' or nombreproducto ILIKE '%" . $filter->text . "%') ".$filterCategorias)
+    	->whereRaw("( codigoproducto::text like '%" . $filter->text . "%' or nombreproducto ILIKE '%" . $filter->text . "%') ".$filterCategorias)
     	->get();
     }
 
